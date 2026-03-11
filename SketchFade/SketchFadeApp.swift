b2f75a906eb9896c57d844894ede596cd6c7336d
Matt Popovich <popovich.matt@gmail.com>
2026-03-11 00:13:29 -0600

    Default project creation

---
+++ SketchFade/SketchFadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SketchFadeApp.swift
+//  SketchFade
+//
+//  Created by Matt Popovich on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SketchFadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
